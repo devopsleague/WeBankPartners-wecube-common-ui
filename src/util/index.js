f9daf367ba36fa1b165a0844a5b91d6dd4112860
wanghao7717 <792974788@qq.com>
2024-08-13 19:58:06 +0800

    问题修复

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,42 @@
+// 防抖函数
+export const debounce1 = (fn, delay) => {
+  let timer = null
+  const that = this
+  return (...args) => {
+    timer && clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(that, args)
+    }, delay)
+  }
+}
+export function debounce(fn, delay = 500) {
+  let timer = null
+  return function () {
+    const args = arguments
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, [...args])
+    }, delay)
+  }
+}
+
+// 截流函数
+export const throttle = (fn, delay) => {
+  let timer = null
+  const that = this
+  return args => {
+    if (timer) {
+      return
+    }
+    timer = setTimeout(() => {
+      fn.apply(that, args)
+      timer = null
+    }, delay)
+  }
+}
+
 // 深拷贝
 export const deepClone = obj => {
   let objClone = Array.isArray(obj) ? [] : {}
